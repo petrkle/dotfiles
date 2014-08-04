12a22bedb7e12505ed0befe4aa6a003f629a67ff
Petr <petr@kle.cz>
2014-08-04 11:06:03 +0200

    Psi - xlock

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-xlock -display :0.0 -timeelapsed -erasemode no_fade -mode juggle +torches +knives +rings +bballs
+dbus-send --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.setStatus string:"away" string:"I am away from my desk. Leave a message."
+xlock -display :0.0 -timeelapsed -erasemode no_fade -mode blank -endCmd 'dbus-send --type=method_call --dest=org.psi-im.Psi /Main org.psi_im.Psi.Main.setStatus string:"online" string:""'
